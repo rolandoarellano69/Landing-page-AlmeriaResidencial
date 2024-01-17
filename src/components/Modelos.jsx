53f4f8cb077f31195fb5d0be238602d7cf84110b
Rolando Arellano <rolandoarellano9869@hotmail.com>
2024-01-16 17:46:32 -0800

    Refactor image paths in Modelos and Modelos2 components

--- src/components/Modelos.jsx
+++ src/components/Modelos.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import { RiCheckboxBlankCircleFill } from "react-icons/ri";
+
 
 function Modelos() {
   return (
-
-    <section id='modelos' className="md:p-8 p-4 relative bg-primary">
-   
-      <img src="/img/Untitled-3-02-01.jpg" alt="fachada almeria" className="hidden md:block rounded-lg shadow-lg h-auto w-auto" />
+    <section id='modelo' class="md:p-8 p-4 container mx-auto relative bg-primary">
       <img src="/img/Untitled-3-02-03.jpg" alt="fachada almeria" className="block md:hidden rounded-lg shadow-lg h-auto w-auto" />
+      <img src="/img/Untitled-3-02-01.jpg" alt="fachada almeria" className="hidden md:block rounded-lg shadow-lg h-auto w-auto" />
 
-  </section>
+    </section>
+ 
 
   )
 }
--- src/components/Modelos2.jsx
+++ src/components/Modelos2.jsx
@@ -2,24 +2,25 @@ import react from 'react'
 
 function Modelos2() {
   return (
-    <section id="modelos" className="p-4 md:p-8 relative bg-primary">
-    <img
-      src="/img/Untitled-3-02-04.jpg"
-      alt="Fachada Almería"
-      className="block md:hidden rounded-lg shadow-lg w-full mb-4"
-    />
+    <section className='md:p-8 p-4 container mx-auto relative bg-primary'>
       <img
-      src="/img/Untitled-3-02-02.jpg"
-      alt="Fachada Almería"
-      className="hidden md:block rounded-lg shadow-lg w-full mb-4"
-    />
- 
+        src="/img/Untitled-3-02-04.jpg"
+        alt="Fachada Almería"
+        className="block md:hidden rounded-lg shadow-lg w-full mb-4"
+      />
       <img
-      src="/img/Untitled-3-02-05.jpg"
-      alt="Fachada Almería"
-      className="block md:hidden rounded-lg shadow-lg w-full mb-4"
-    />
-  </section>
+        src="/img/Untitled-3-02-02.jpg"
+        alt="Fachada Almería"
+        className="hidden md:block rounded-lg shadow-lg w-full mb-4"
+      />
+
+      <img
+        src="/img/Untitled-3-02-05.jpg"
+        alt="Fachada Almería"
+        className="block md:hidden rounded-lg shadow-lg w-full mb-4"
+      />
+    </section>
+
 
   )
 }
